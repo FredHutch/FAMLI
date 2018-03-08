20e4bc42e277afba6805673e28ac01e0dda22df3
Sam Minot <sam@fredhutch.org>
2018-03-07 16:33:01 -0800

    Add one testcase for completeness

--- famli/test_alignment_batchsize.py
+++ famli/test_alignment_batchsize.py
@@ -12,7 +12,7 @@ assert len(alignments) == 100468, len(alignments)
 
 # Now read the alignments in smaller batches, and make sure
 # all of them make it through
-for batchsize in [10, 20, 50, 100, 1e7]:
+for batchsize in [10, 20, 50, 100, 1e7, None]:
     with open("/usr/famli/tests/example.diamond.aln", "rt") as f:
         chunks = [a for a in parser.yield_alignments(f, batchsize=batchsize)]
     assert sum(map(len, chunks)) == len(alignments), chunks
