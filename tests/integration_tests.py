5e42dbc05813902a0220cd4ac9d75fee1fc29673
Jonathan Golob <jgolob@rhino3.fhcrc.org>
2018-03-28 13:13:54 -0700

    Changed the expected number of deduplicated reads for the second test

--- tests/integration_tests.py
+++ tests/integration_tests.py
@@ -55,7 +55,7 @@ output = json.load(gzip.open("/usr/famli/tests/example2.json.gz"))
 assert output["aligned_reads"] == 338, output["aligned_reads"]
 assert output["total_reads"] == 360, output["total_reads"]
 assert output["ref_db"] == "/usr/famli/tests/refdb.dmnd", output["ref_db"]
-assert output["deduplicated_reads"] == 253, output["deduplicated_reads"]
+assert output["deduplicated_reads"] == 155, output["deduplicated_reads"]
 assert output["output_folder"] == "/usr/famli/tests", output["output_folder"]
 assert output["input_path"] == "/usr/famli/tests/example.fastq", output["input_path"]
 
