814809620e0a50b45aa7e3187fe98a15afd12d6e
Sam Minot <sam@fredhutch.org>
2020-02-23 09:02:05 -0800

    Update version to 1.2

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ with open(path.join(here, 'README.md'), encoding='utf-8') as f:
 
 setup(
     name='famli',
-    version='1.1',
+    version='1.2',
     description='Functional Analysis of Metagenomes by Likelihood Inferrence',
     long_description=long_description,
     long_description_content_type='text/markdown',
